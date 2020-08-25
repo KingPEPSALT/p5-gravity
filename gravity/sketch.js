65fa482da827e111c4f21e6573e39342186f66af
KingPEPSALT <quinmackfoster+github@gmail.com>
2020-08-25 02:34:54 +0100

    user interaction, can add force to a planet and create planets

--- gravity/sketch.js
+++ gravity/sketch.js
@@ -5,7 +5,7 @@ const CANVAS_Y = 1200;
 const FONTSIZE = 20;
 let showinfo = false;
 let showtrails = false;
-
+let creatingBody = false;
 //PHYSICS CONSTANTS
 const G = 6.674*(10**-11);
 
@@ -18,11 +18,11 @@ function dTime(){
 }
 
 function forceBetween(bodyA, bodyB){
-  return G * ( ( bodyA.mass * bodyB.mass ) / distanceBetween( bodyA , bodyB ) );
+  return G * ( ( bodyA.mass * bodyB.mass ) / distanceBetween( bodyA.position , bodyB.position ) );
 }  
 
-function distanceBetween(bodyA, bodyB){
-   return p5.Vector.mag(p5.Vector.sub(bodyA.position, bodyB.position));
+function distanceBetween(veca, vecb){
+   return p5.Vector.mag(p5.Vector.sub(veca, vecb));
 }
 
 let bodies = []
@@ -40,19 +40,25 @@ class Body{
     this.name = name
     this.mincutoff = mincutoff;
     this.trail = [];
+    this.selected = false;
+    this.addedForce = [];
+    
     bodies.push(this);
   }
     
   update(){
-    let accelerationsActing = []
+    let accelerationsActing = [];
+    let resultantAdded = createVector(0,0);
+    for(let accel of this.addedForce) resultantAdded.add(accel);
+    this.addedForce = [];
     for(let body of bodies){
       let gAcceleration = p5.Vector.sub(body.position, this.position).setMag((forceBetween(this, body)/this.mass));
       this.velocity.add(gAcceleration);
       if(this.name === "SUN  ") print(this.velocity.mag());
       accelerationsActing.push(gAcceleration);
     }
+    this.velocity.add(resultantAdded);
     this.position.add(this.trueVelocity());
-
     if(showinfo){
       for(let a of accelerationsActing){
         if(a != undefined) drawArrow(this.position, a, color(0, 0, 255), this.radius);
@@ -68,14 +74,21 @@ class Body{
     stroke(this.colour);
     strokeWeight(2);
     if(showtrails) for(let t of this.trail) point(t.x, t.y);
-    noStroke();
-    ellipse(this.position.x, this.position.y, this.radius*2);
+    strokeWeight(7);
+    stroke(color(255));
+    if(!this.mouseInside()) noStroke();
+    ellipse(this.position.x, this.position.y, this.radius);
     fill(fillbuffer);
     stroke(strokebuffer);
     strokeWeight(strokeweightbuffer);
     
   }
-  
+  addForce(force){
+    this.addedForce.push(p5.Vector.div(force, this.mass));
+  }
+  mouseInside(){
+    return !(distanceBetween(createVector(mouseX, mouseY), this.position) > this.radius/2);
+  }
   // PROPERTIES
   
   // base velocity properties
@@ -105,11 +118,11 @@ function setup() {
   frameRate(FRAMERATE);
   textFont('Courier New');
   textSize(FONTSIZE);
-  let earth = new Body(createVector(300, 1100), 4000000, 25, createVector(-100, -30), color(26, 188, 156), "EARTH");
-  let sun = new Body(createVector(600, 600), 6000000000000, 90, createVector(0,0), color(255,255,0), "SUN", true);
-  let mars = new Body(createVector(800, 1200), 5000000, 40, createVector(50, -40), color(188, 42, 58), "MARS");
-  let jack = new Body(createVector(400, 200), 7000000, 55, createVector(80, 30), color(105, 105, 105), "JACK");
-  let kashiyuka = new Body(createVector(200, 200), 6000000, 47, createVector(100, 20), color(0, 197, 255), "KASHIYUKA");
+  let earth = new Body(createVector(300, 1100), 4000000, 50, createVector(-100, -30), color(26, 188, 156), "EARTH");
+  let sun = new Body(createVector(600, 600), 6000000000000, 180, createVector(0,0), color(255,255,0), "SUN", true);
+  let mars = new Body(createVector(800, 1200), 5000000, 80, createVector(50, -40), color(188, 42, 58), "MARS");
+  let jack = new Body(createVector(400, 200), 7000000, 110, createVector(80, 30), color(105, 105, 105), "JACK");
+  let kashiyuka = new Body(createVector(200, 200), 6000000, 94, createVector(100, 20), color(0, 197, 255), "KASHIYUKA");
 }
 
 function draw() {
@@ -136,7 +149,20 @@ function draw() {
     stroke(strokebuffer);
     strokeWeight(strokeweightbuffer);
   }
+  if(creatingBody){
+    diameter += deltaTime;
+    fill(colour);
 
+    ellipse(mouseX, mouseY, diameter/2);
+    fill(fillbuffer);
+  }
+  if(selected){
+    stroke(255, 0, 0);
+    strokeWeight(4);
+    line(bodyActedUpon.position.x, bodyActedUpon.position.y, mouseX, mouseY);
+    stroke(strokebuffer);
+    strokeWeight(strokeweightbuffer);
+  }
 }
 function drawArrow(base, vec, col, min, mincutoff = false) {
   push();
@@ -149,7 +175,7 @@ function drawArrow(base, vec, col, min, mincutoff = false) {
   translate(base.x, base.y);
 
   // minimum formatting work
-  let conditional = arrowVec.mag() < (min+arrowSize);
+  let conditional = (arrowVec.mag() < (min+arrowSize)) && (arrowVec.mag() > 0);
   if(mincutoff) conditional = conditional && (arrowVec.mag() > 1);
   if(conditional) arrowVec.setMag(min+arrowSize);
 
@@ -166,4 +192,37 @@ function drawArrow(base, vec, col, min, mincutoff = false) {
 function keyPressed(){
   if(keyCode == 72) showinfo = !showinfo;
   if(keyCode == 84) showtrails = !showtrails;
+}
+
+var diameter;
+var colour;
+var bodyActedUpon;
+var selected;
+function mousePressed(){
+  selected = false;
+  for(let body of bodies){
+    if(body.mouseInside()){
+      bodyActedUpon = body;
+      original = body.position.copy();
+      selected = true;
+      break;
+    }
+  }
+  if(!creatingBody){
+    creatingBody = !selected;
+    diameter = 0;
+    colour = color(random(0, 255), random(0, 255), random(255));
+  }
+  
+  
+}
+function mouseReleased(){
+  creatingBody = false;
+  
+  if(!selected) new Body(createVector(mouseX, mouseY), diameter*127272.727, diameter, createVector(0,0), colour, "CLICKED");
+  else {
+    let dist = p5.Vector.mult(p5.Vector.sub(bodyActedUpon.position, createVector(mouseX, mouseY)));
+    bodyActedUpon.addForce(p5.Vector.mult(dist, bodyActedUpon.mass/2));
+  }
+  selected = false;
 }
